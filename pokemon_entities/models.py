780fd2567870ffb3065d83bf483105b61fe996e0
Polina Vasilyeva <Polinova31@gmail.com>
2026-01-09 20:46:15 +0300

    create Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,5 @@
-from django.db import models  # noqa F401
+from django.db import models  
 
 class Pokemon(models.Model):
     title = models.CharField(
-        max_length=100)
+        max_length=100) 
