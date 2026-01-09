2b2790f1bb540a026c3c2fadbef9753559562e40
Polina Vasilyeva <Polinova31@gmail.com>
2026-01-09 22:44:21 +0300

    add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,5 +5,11 @@ class Pokemon(models.Model):
         max_length=200,
         verbose_name='Pokemon name',
     )
+    pokemon_image = models.ImageField(
+        upload_to='media',
+        verbose_name='Images',
+        null=True,
+        blank=True,
+    )
     def __str__(self):
         return f'{self.title}'
