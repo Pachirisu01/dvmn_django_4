dea0c742c7ed9e846da14646981175af539d1f18
Polina Vasilyeva <Polinova31@gmail.com>
2026-05-13 12:10:43 +0300

    Add translations of the Pokemon's name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,18 @@ class Pokemon(models.Model):
         max_length=200,
         verbose_name='Pokemon name',
     )
+    title_en = models.CharField(
+        max_length=200,
+        verbose_name='English name',
+        blank=True,
+        null=True,
+    )
+    title_jp = models.CharField(
+        max_length=200,
+        verbose_name='Japanese name',
+        blank=True,
+        null=True,
+    )
     pokemon_image = models.ImageField(
         upload_to='media',
         verbose_name='Images',
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -107,6 +107,8 @@ def show_pokemon(request, pokemon_id):
     pokemon_data = {
         'pokemon_id': pokemon.id,
         'title_ru': pokemon.title,
+        'title_en': pokemon.title_en,
+        'title_jp': pokemon.title_jp,
         'img_url': img_final,
         'description': pokemon.description,
     }
